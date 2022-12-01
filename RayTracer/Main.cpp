4768c1a8fe367141654ef1e0d79f3868baa32342
KBear100 <kgamett@student.neumont.edu>
2022-12-01 16:27:33 -0700

    Canvas class and static

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -5,19 +5,41 @@ int main(int, char**)
 {
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(400, 300);
+	renderer.CreateWindow(600, 400);
+
+	Canvas canvas(600, 400, renderer);
 
 	bool quit = false;
 	while (!quit) 
 	{
 		SDL_Event event;
 		SDL_PollEvent(&event);
+
 		switch (event.type)
 		{
 		case SDL_QUIT:
 			quit = true;
 			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
 		}
-	}
+
+		// render scene
+		canvas.Clear({ 0, 0, 0, 1});
+		for (int i = 0; i < 100000; i++)
+		{
+			canvas.DrawPoint({ random(0, 600), random(0, 400)}, {1, 1, 1, 1});
+		}
+		canvas.Update();
+
+		renderer.CopyCanvas(canvas);
+		renderer.Present();
+	} 
 	renderer.Shutdown(); return 0;
 }
